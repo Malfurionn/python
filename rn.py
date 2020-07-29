77ba26d016a7033622c73bb5166dafd4bd42162f
Nima <nima.omid@gmail.com>
2020-07-29 14:51:36 +0200

    Initial project version

---
+++ rn.py
@@ -0,0 +1,12 @@
+import random
+
+b = 0
+while True:
+    i = random.randint(10000, 99999)
+    if i != 69420:
+        print(i)
+        b += 1
+    else:
+        print(i)
+        break
+print("Done in " + str(b) + " tries")
